e4eb036ccdd48c62646804aa7824a928642903e9
sikander-nawaz <sikandernawaz295@gmail.com>
2025-07-24 10:43:31 +0500

    added

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Subjects from './Subjects'
 import HeroSection from './HeroSection'
-import Feature from './Feature'
 import FeaturedTutors from './FeaturedTutors'
 import HowItWorks from './HowItWorks'
 import { Stats } from './stats'
@@ -13,7 +12,6 @@ const Home = () => {
   return (
    <>
    <HeroSection/>
-   {/* <Feature/> */}
    <Subjects/>
    <FeaturedTutors/>
    <Stats/>
