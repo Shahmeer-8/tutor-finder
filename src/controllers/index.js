43ea645af85d5deb9aed65f51e5fb13db91ee12e
sikander-nawaz <sikandernawaz295@gmail.com>
2026-04-03 11:24:09 +0500

    'start-project'

--- src/controllers/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  authController: require("./auth.controller"),
-  requestController: require("./request.controller"),
-  paymentController: require("./payment.controller"),
-  reviewController: require("./review.controller"),
-  tutorController: require("./tutor.controller"),
-};
--- src/routes/v1/tutor.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { tutorController } = require('../../controllers');
-
-const router = express.Router();
-
-router.get('/search', tutorController.searchTutors);
-
-module.exports = router;
