77dd9919ac8164584acafe0b3686c0886a6d51aa
Shahmeer-8 <shah.meer@multilynx.pk>
2026-06-16 01:27:42 +0500

    fix: update remaining @/pages/TutorDetail import to @/views/TutorDetail

--- frontend/app/tutors/[id]/page.tsx
+++ frontend/app/tutors/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Layout } from "@/components/layout/Layout";
-import TutorDetail from "@/pages/TutorDetail";
+import TutorDetail from "@/views/TutorDetail";
 export default function Page() {
   return <Layout><TutorDetail /></Layout>;
 }
